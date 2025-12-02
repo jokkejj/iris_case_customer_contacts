c45b989b0a49ab3ab9c585f7a02523afdd749b88
jokkejj <120713843+jokkejj@users.noreply.github.com>
2025-12-02 18:16:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="iris_case_customer_contacts",
-    version="1.0",
+    version="1.0.0",
     packages=find_packages(),
+    install_requires=[
+        "iris-module-interface>=1.1,<1.3",
+    ],
 )
